1c11196c883af56fbb86e551d34d72158af9540a
rokko <rocco.caricola89@gmail.com>
2022-02-24 23:02:18 +0100

    port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require ('express')
 const app = express()
 const http = require('http').createServer(app);
-http.listen(436)
+http.listen(443)
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const jwt = require('jsonwebtoken')
