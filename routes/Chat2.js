adbe1e967fd33bbd5596e080a765894bec2bcf31
rokko <rocco.caricola89@gmail.com>
2022-03-19 18:08:43 +0100

    node_modules be gone

---
+++ routes/Chat2.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const Contatto = require('../models/Contatti')
+const Message = require('../models/Message')
+const auth = require('../middlewares/login')
+const router = express()
+
+router.post('/get-message', auth, async(req,res,next)=>{
+
+    const listaMessaggi= await Message.find({contatti_id:req.body.contatti_id})
+
+    res.send(listaMessaggi)
+    
+})
+
+router.post('/send-message',auth, async(req,res,next)=>{
+    const nuovoMessaggio =await new Message({
+        sender: req.user._id,
+        contatti_id: req.body.contatti_id,
+        message: req.body.message
+    })
+
+    await nuovoMessaggio.save()
+
+    res.send(nuovoMessaggio)
+
+})
+
+module.exports=router
