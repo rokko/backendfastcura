ebb6e56ccbbdd1af484c51fb63c8d279c4dbd9c5
Rocco Caricola <rocco.caricola89@gmail.com>
2022-05-26 15:12:03 +0200

    id

--- routes/Chat2.js
+++ routes/Chat2.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const Contatto = require("../models/Contatti");
 const Message = require("../models/Message");
+const Cliente = require("../models/Clienti");
+const Professionista = require("../models/Professionista");
 const auth = require("../middlewares/login");
 const router = express();
+const nodemailer = require("nodemailer");
+
+let transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  auth: {
+    user: "fastcura.responding@gmail.com",
+    pass: "Accountprova1",
+  },
+});
 
 router.post("/get-message", async (req, res, next) => {
   const listaMessaggi = await Message.find({
@@ -33,6 +45,26 @@ router.post("/send-message", auth, async (req, res, next) => {
     ricreader: false,
   });
 
+  const contatto = await Contatto.findOne({ _id: req.body.contatti_id });
+  if (contatto.id_cliente === req.user._id) {
+    const utenteDaRicevere = await Professionista.findOne({
+      _id: contatto.id_professionista,
+    });
+    const message = {
+      from: "from-example@email.com",
+      to: utenteDaRicevere.email,
+      subject: "Hai ricevuto un nuovo messaggio",
+      text: `Ciao ${utenteDaRicevere.nome} ${utenteDaRicevere.cognome}  , ti è appena arrivato un messaggio su Fastcura , dai un'occhiata`,
+    };
+
+    transporter.sendMail(message, (err, info) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(info);
+      }
+    });
+  }
   await nuovoMessaggio.save();
 
   res.send(nuovoMessaggio);
