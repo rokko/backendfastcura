a6051ef7b2409a20ccc48d13b2aeaec55db317d0
Rocco Caricola <rocco.caricola89@gmail.com>
2023-02-23 20:34:51 +0100

    aggiunta data

--- models/Professionista.js
+++ models/Professionista.js
@@ -1,32 +1,30 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const ProfessionistaSchema = mongoose.Schema({
-    nome: String,
-    cognome: String,
-    email : String,
-    password : String,
-    datadinascita : Date,
-    sesso : String,
-    citta : String,
-    number : String,
-    professione : String,
-    greenpass : String,
-    eta : String,
-    referenze : String,
-    annitalia : String,
-    esperienza : String,
-    motivotermine: String,
-    livelloitaliano: String,
-    titolodistudio : String,
-    possessodilaurea: String,
-    numeroiscrizione : String,
-    assicurato : String,
-    precedente : String,
-    abbonamento: String,
-    scadenza : Date,
+  nome: String,
+  cognome: String,
+  email: String,
+  password: String,
+  datadinascita: Date,
+  sesso: String,
+  citta: String,
+  number: String,
+  professione: String,
+  greenpass: String,
+  eta: String,
+  referenze: String,
+  annitalia: String,
+  esperienza: String,
+  motivotermine: String,
+  livelloitaliano: String,
+  titolodistudio: String,
+  possessodilaurea: String,
+  numeroiscrizione: String,
+  assicurato: String,
+  precedente: String,
+  abbonamento: String,
+  scadenza: Date,
+  dataIscrizione: Date,
+});
 
-})
-
-
-
-module.exports = mongoose.model('Professionista', ProfessionistaSchema)
+module.exports = mongoose.model("Professionista", ProfessionistaSchema);
