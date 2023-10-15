3fa26e7791b0423e29e7799333e3bb6d5aa07788
Rocco Caricola <rocco.caricola89@gmail.com>
2023-10-15 23:18:25 +0200

    correzione string

--- models/Segnalazione.js
+++ models/Segnalazione.js
@@ -4,7 +4,7 @@ const SegnalazioneSchema = mongoose.Schema({
     data: Date,
     citta : String,
     professione: String,
-    email: string,
+    email: String,
 })
 
 module.exports = mongoose.model('Segnalazione', SegnalazioneSchema)
